fce69771c5ea592f81d3f1a55bb0b914da66dd62
Carlos Guerrero <git@carlosguerrero.com>
2019-04-14 16:38:12 +0300

    Remove example user

--- sso.js
+++ sso.js
@@ -10,53 +10,7 @@ fetch("https://auth.ruso-facil.com/user.json", {
   cache: "no-cache"
 })
   .then(function(response) {
-    // return response.json();
-    return {
-      authenticated: true,
-      captcha: false,
-      sso: {
-        facebook: {
-          id: "10156520253447730",
-          displayName: "Carlos Guerrero",
-          name: {},
-          emails: [
-            {
-              value: "guerrerocarlos@gmail.com"
-            }
-          ],
-          photos: [
-            {
-              value:
-                "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10156520253447730&height=50&width=50&ext=1557839797&hash=AeRDXkx4XPkS4dvY"
-            }
-          ],
-          provider: "facebook",
-          _raw:
-            '{"id":"10156520253447730","email":"guerrerocarlos\\u0040gmail.com","name":"Carlos Guerrero","picture":{"data":{"height":50,"is_silhouette":false,"url":"https:\\/\\/platform-lookaside.fbsbx.com\\/platform\\/profilepic\\/?asid=10156520253447730&height=50&width=50&ext=1557839797&hash=AeRDXkx4XPkS4dvY","width":50}}}',
-          _json: {
-            id: "10156520253447730",
-            email: "guerrerocarlos@gmail.com",
-            name: "Carlos Guerrero",
-            picture: {
-              data: {
-                height: 50,
-                is_silhouette: false,
-                url:
-                  "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10156520253447730&height=50&width=50&ext=1557839797&hash=AeRDXkx4XPkS4dvY",
-                width: 50
-              }
-            }
-          }
-        }
-      },
-      __sso: {
-        facebook: {
-          accessToken:
-            "EAAdkOuLCXfwBAGfCcTrnoGs4HRaSawhdzZB1nQnuWBZAk8diBBV7w0opwJFESgaMlClANbXAYxtw7RSDPuGnBmaWryl8chmPtrPAPw5lgAtFiEGH0Y09RbrxZAdT4hNKO9sTflrx7JRehpxbQHiZBL7aJbZAZArEd2jLBgin6hn0VxzCcx1vTh"
-        }
-      },
-      storage: {}
-    };
+    return response.json();
   })
   .then(user => {
     console.log("USER", user);
